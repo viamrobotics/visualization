f816b5cc5a07e5bd8ffadb03d92b339c9f6b9a29
Micheal Parks <micheal.parks@outlook.com>
2025-09-11 13:19:10 -0400

    use vite worker bundling

--- src/lib/hooks/useWorldState.svelte.ts
+++ src/lib/hooks/useWorldState.svelte.ts
@@ -15,6 +15,7 @@ import { usePartID } from './usePartID.svelte'
 import { setInUnsafe } from '@thi.ng/paths'
 import type { ProcessMessage } from '$lib/world-state-messages'
 import { getContext, setContext } from 'svelte'
+import WorldStateWorker from '../workers/worldStateWorker?worker'
 
 const key = Symbol('world-state-context')
 
@@ -23,9 +24,7 @@ interface Context {
 	current: Record<string, ReturnType<typeof createWorldState>>
 }
 
-const worker = new Worker(new URL('../workers/worldStateWorker.ts', import.meta.url), {
-	type: 'module',
-})
+const worker = new WorldStateWorker()
 
 export const provideWorldStates = () => {
 	const partID = usePartID()
