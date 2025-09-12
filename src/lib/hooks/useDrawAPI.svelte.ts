471db27af82c81c966b24c116bbd8f8c28c44c72
Micheal Parks <micheal.parks@outlook.com>
2025-09-12 13:44:44 -0400

    cleanup

--- src/lib/hooks/useDrawAPI.svelte.ts
+++ src/lib/hooks/useDrawAPI.svelte.ts
@@ -218,8 +218,6 @@ export const provideDrawAPI = () => {
 			label += String.fromCharCode(reader.read())
 		}
 
-		console.log(label)
-
 		const index = points.findIndex(({ name }) => name === label)
 		if (index !== -1) {
 			points.splice(index, 1)
