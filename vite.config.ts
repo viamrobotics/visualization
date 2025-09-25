27a572ecbf80fca4a0e681c41b23f9530826d371
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2025-09-25 11:42:29 -0400

    Revert "Worker fixes (#116)"
    This reverts commit ad0c509d49f7cec6bd3d6c8d281179131bb95a85.

--- vite.config.ts
+++ vite.config.ts
@@ -27,10 +27,6 @@ export default defineConfig({
 		sveltekit(),
 	],
 
-	worker: {
-		format: 'es',
-	},
-
 	define: {
 		BACKEND_IP: JSON.stringify(localIP),
 		BUN_SERVER_PORT: JSON.stringify(process.env.BUN_SERVER_PORT),
