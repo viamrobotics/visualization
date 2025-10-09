94e50f6fc42fd0372a064d0ecf37c68901a10fa3
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2025-10-09 10:43:21 -0400

    remove log

--- src/lib/hooks/usePartConfig.svelte.ts
+++ src/lib/hooks/usePartConfig.svelte.ts
@@ -451,7 +451,6 @@ export class StandalonePartConfig implements LocalPartConfig {
 		return this._localPartConfig ?? new Struct()
 	}
 	public setLocalPartConfig(config: Struct): void {
-		console.log('setLocalPartConfig', config.toJson())
 		this._localPartConfig = config
 		this._isDirty = true
 	}
