fbd2727ec4ffc2e4086c9f760d9e4b60c34beaec
Micheal Parks <micheal.parks@outlook.com>
2025-12-11 16:29:20 -0500

    tree fixes

--- src/lib/components/Tree/TreeContainer.svelte
+++ src/lib/components/Tree/TreeContainer.svelte
@@ -65,7 +65,7 @@
 {#if draggable.isLoaded && resizable.isLoaded}
 	<div
 		bind:this={container}
-		class="bg-extralight border-medium absolute top-0 left-0 z-1000 m-2 w-60 overflow-y-auto border text-xs"
+		class="bg-extralight border-medium absolute top-0 left-0 z-1000 m-2 resize overflow-y-auto border text-xs"
 		style:transform="translate({draggable.current.x}px, {draggable.current.y}px)"
 		style:width={resizable.current ? `${resizable.current.width}px` : undefined}
 		style:height={resizable.current ? `${resizable.current.height}px` : undefined}
