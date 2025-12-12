f64fa6ba58c03b846caff5cb0ef983d6474672c2
Devin C <devin@viam.com>
2025-12-12 11:35:49 -0500

    move env vars to vite-env file to avoid build conflicts

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,10 +3,6 @@
 import type { InteractivityProps, BVHProps } from '@threlte/extras'
 
 declare global {
-	// Vite define replacements (set at build time via vite.config.ts)
-	const BACKEND_IP: string
-	const WS_PORT: string
-
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
