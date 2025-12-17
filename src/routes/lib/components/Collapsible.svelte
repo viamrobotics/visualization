5b23b665ab909f5639bdcb0a48787f5ae4063d8c
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2025-12-17 16:23:02 -0500

    Improve machine picker UI (#315)
    * improve machine-picker
    
    * Create afraid-candles-listen.md
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * add aria-label
    
    * accessible close

---
+++ src/routes/lib/components/Collapsible.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Icon } from '@viamrobotics/prime-core'
+	import * as collapsible from '@zag-js/collapsible'
+	import { normalizeProps, useMachine } from '@zag-js/svelte'
+
+	let { children } = $props()
+
+	const id = $props.id()
+	const service = useMachine(collapsible.machine, { id })
+	const api = $derived(collapsible.connect(service, normalizeProps))
+</script>
+
+<div>
+	<button
+		{...api.getTriggerProps()}
+		class="bg-gray-2 m-auto flex w-14 justify-center"
+		aria-label="Expand connection config"
+	>
+		<Icon
+			name="chevron-down"
+			cx={api.open ? 'rotate-180' : ''}
+		/>
+	</button>
+
+	<div {...api.getContentProps()}>
+		{@render children()}
+	</div>
+</div>
