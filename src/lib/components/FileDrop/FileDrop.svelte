cf2d2990e10daa3519bd4cc673acb67cf7462bac
Micheal Parks <micheal.parks@outlook.com>
2026-01-02 13:08:34 -0500

    fix camera pose setting

--- src/lib/components/FileDrop/FileDrop.svelte
+++ src/lib/components/FileDrop/FileDrop.svelte
@@ -6,17 +6,32 @@
 	import type { FileDropperSuccess } from './file-dropper'
 	import { traits } from '$lib/ecs'
 	import { spawnSnapshotEntities } from '$lib/snapshot'
+	import { useCameraControls } from '$lib/hooks/useControls.svelte'
 
 	const props: HTMLAttributes<HTMLDivElement> = $props()
 
 	const world = useWorld()
 	const toast = useToast()
+	const cameraControls = useCameraControls()
 
 	const fileDrop = useFileDrop(
 		(result: FileDropperSuccess) => {
 			switch (result.type) {
 				case 'snapshot': {
 					spawnSnapshotEntities(world, result.snapshot)
+
+					const { sceneCamera } = result.snapshot.sceneMetadata ?? {}
+
+					if (sceneCamera) {
+						const { x = 0, y = 0, z = 0 } = sceneCamera.position ?? {}
+						const { x: lx = 0, y: ly = 0, z: lz = 0 } = sceneCamera.lookAt ?? {}
+
+						cameraControls.setPose({
+							position: [x * 0.001, y * 0.001, z * 0.001],
+							lookAt: [lx * 0.001, ly * 0.001, lz * 0.001],
+						})
+					}
+
 					break
 				}
 				case 'pcd':
