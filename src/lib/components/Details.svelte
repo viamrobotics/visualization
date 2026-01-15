7d66c9d33fd73a0bebf158be6775aee9bd7823c7
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-01-15 14:08:01 -0500

    add gripper to origin_frame compoennt (#346)
    * add gripper to origin_frame compoennt
    
    * changeset
    
    ---------
    
    Co-authored-by: Micheal Parks <103450731+micheal-parks@users.noreply.github.com>

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -31,10 +31,7 @@
 
 	const { ...rest } = $props()
 
-	const dragPosition = new PersistedState<Vector2Like | undefined>(
-		'details-drag-position',
-		undefined
-	)
+	const dragPosition = new PersistedState<Vector2Like>('details-drag-position', { x: 0, y: 0 })
 
 	const resourceByName = useResourceByName()
 	const frames = useFrames()
