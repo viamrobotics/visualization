874b1a988ad6777c454569927d39277122ad4d05
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-02-09 15:18:33 -0500

    clean up hover updater types

--- src/lib/HoverUpdater.svelte.ts
+++ src/lib/HoverUpdater.svelte.ts
@@ -3,24 +3,22 @@ import type { Entity } from 'koota'
 import { traits } from './ecs/index.ts';
 import type { IntersectionEvent } from '@threlte/extras';
 
-export interface ClosestArrow {
+export interface HoverInfo {
     index: number
-    x: number
-    y: number
-    z: number
-    oX: number
-    oY: number
-    oZ: number
+    position: {
+        x: number
+        y: number
+        z: number
+    }
+    orientation?: {
+        x: number
+        y: number
+        z: number
+    }
+    
 }
 
-export interface ClosestPoint {
-    index: number
-    x: number
-    y: number
-    z: number
-}
-
-export const getClosestArrow = (positions: Float32Array, point: Vector3): ClosestArrow => {
+export const getClosestArrow = (positions: Float32Array, point: Vector3): HoverInfo => {
     let smallestDistance = Infinity
     let index = -1
 
@@ -39,16 +37,20 @@ export const getClosestArrow = (positions: Float32Array, point: Vector3): Closes
 
     return {
         index: Math.floor(index / 6),
-        x: positions[index] / 1000,
-        y: positions[index + 1] / 1000,
-        z: positions[index + 2] / 1000,
-        oX: positions[index + 3],
-        oY: positions[index + 4],
-        oZ: positions[index + 5],
+        position: {
+            x: positions[index] / 1000,
+            y: positions[index + 1] / 1000,
+            z: positions[index + 2] / 1000,
+        },
+        orientation: {
+            x: positions[index + 3],
+            y: positions[index + 4],
+            z: positions[index + 5]
+        }
     }
 }
 
-export const getClosestPoint = (positions: Float32Array, point: Vector3): ClosestPoint => {
+export const getClosestPoint = (positions: Float32Array, point: Vector3): HoverInfo => {
     let smallestDistance = Infinity
     let index = -1
 
@@ -67,55 +69,47 @@ export const getClosestPoint = (positions: Float32Array, point: Vector3): Closes
 
     return {
         index: Math.floor(index / 3),
-        x: positions[index],
-        y: positions[index + 1],
-        z: positions[index + 2],
+        position: {
+            x: positions[index],
+            y: positions[index + 1],
+            z: positions[index + 2],
+        }
     }
 }
 
-export const getPointAtIndex = (positions: Float32Array, index: number): ClosestPoint | null => {
+export const getPointAtIndex = (positions: Float32Array, index: number): HoverInfo | null => {
     if (index < 0 || index >= positions.length / 3)
     {
         return null
     }
     return {
         index,
-        x: positions[index * 3],
-        y: positions[index * 3 + 1],
-        z: positions[index * 3 + 2],
+        position: {
+            x: positions[index * 3],
+            y: positions[index * 3 + 1],
+            z: positions[index * 3 + 2],
+        }
     }
 }
-export const getArrowAtIndex = (positions: Float32Array, index: number): ClosestArrow | null => {
+export const getArrowAtIndex = (positions: Float32Array, index: number): HoverInfo | null => {
     if (index < 0 || index >= positions.length / 6)
     {
         return null
     }
     return {
         index,
-        x: positions[index * 6]/ 1000,
-        y: positions[index * 6 + 1]/ 1000,
-        z: positions[index * 6 + 2]/ 1000,
-        oX: positions[index * 6 + 3],
-        oY: positions[index * 6 + 4],
-        oZ: positions[index * 6 + 5],
+        position: {
+            x: positions[index * 6]/ 1000,
+            y: positions[index * 6 + 1]/ 1000,
+            z: positions[index * 6 + 2]/ 1000,
+        },
+        orientation: {
+            x: positions[index * 6 + 3],
+            y: positions[index * 6 + 4],
+            z: positions[index * 6 + 5],
+        }
     }
 }
-
-export interface HoverInfo {
-    index: number
-    position: {
-        x: number
-        y: number
-        z: number
-    }
-    orientation?: {
-        x: number
-        y: number
-        z: number
-    }
-    
-}
-
 
 export const updateHoverInfo = (entity: Entity, hoverEvent: IntersectionEvent<MouseEvent>): HoverInfo | null => {
     const { index, point } = hoverEvent
@@ -130,13 +124,13 @@ export const updateHoverInfo = (entity: Entity, hoverEvent: IntersectionEvent<Mo
     if (entity.has(traits.Arrows)) {
         const closestArrow = getClosestArrow(entity.get(traits.Positions) as Float32Array, hoverPosition)
         if (closestArrow) {
-            hoverInfo = { index: closestArrow.index, position: { x: closestArrow.x, y: closestArrow.y, z: closestArrow.z }, orientation: { x: closestArrow.oX, y: closestArrow.oY, z: closestArrow.oZ } }
+            hoverInfo = closestArrow
         }
     } else if (entity.has(traits.Points)) {
         const positions = entity.get(traits.BufferGeometry)?.attributes.position.array as Float32Array
         const closestPoint = getClosestPoint(positions, hoverPosition)
         if (closestPoint) {
-            hoverInfo = { index: closestPoint.index, position: { x: closestPoint.x, y: closestPoint.y, z: closestPoint.z } }
+            hoverInfo = closestPoint
         }
     }
 
@@ -148,13 +142,13 @@ export const getLinkedHoverInfo = (index: number, linkedEntity: Entity): HoverIn
     if (linkedEntity.has(traits.Arrows)) {
         const closestArrow = getArrowAtIndex(linkedEntity.get(traits.Positions) as Float32Array, index)
         if (closestArrow) {
-            return { index: closestArrow.index, position: { x: closestArrow.x, y: closestArrow.y, z: closestArrow.z }, orientation: { x: closestArrow.oX, y: closestArrow.oY, z: closestArrow.oZ } }
+            return closestArrow
         }
     } else if (linkedEntity.has(traits.Points)) {
         const positions = linkedEntity.get(traits.BufferGeometry)?.attributes.position.array as Float32Array
         const closestPoint = getPointAtIndex(positions, index)
         if (closestPoint) {
-            return { index: closestPoint.index, position: { x: closestPoint.x, y: closestPoint.y, z: closestPoint.z } }
+            return closestPoint
         }
     }
 
