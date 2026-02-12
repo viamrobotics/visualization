3022029e18219de23012984a3e13c0339b530ed0
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-02-12 12:58:39 -0500

    App 15105 create entity relationships (#379)
    * feat: hover link relationship
    
    * allow specifying index mapping in ui
    
    * move link hovers to a context
    
    * clean up hover updater types
    
    * move hover components to their own folder
    
    * update to use the useTrait hook
    
    * lint
    
    * stop prop on indexMapping keypress
    
    * pull AddRelationship secion out of Details
    
    * cleanup
    
    * revert package.json removals, pnpm i
    
    * format check
    
    * only show relationship button for pcds and arrows for now
    
    * changeset
    
    * fix imports
    
    * use pooled vec3
    
    * address comments
    
    * add hover trait in and add prop for link type
    
    * clean
    
    * rename ox -> oX
    
    ---------
    
    Co-authored-by: Micheal Parks <micheal.parks@outlook.com>

---
+++ src/lib/components/hover/HoveredEntities.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { traits, useTrait } from '$lib/ecs'
+	import { useSelectedEntity } from '$lib/hooks/useSelection.svelte'
+	import { useFocusedEntity } from '$lib/hooks/useSelection.svelte'
+	import HoveredEntity from './HoveredEntity.svelte'
+	import LinkedHoveredEntity from './LinkedHoveredEntity.svelte'
+	import { useLinkedEntities } from '$lib/hooks/useLinked.svelte'
+
+	const selectedEntity = useSelectedEntity()
+	const focusedEntity = useFocusedEntity()
+	const linkedEntities = useLinkedEntities()
+
+	const displayEntity = $derived(selectedEntity.current ?? focusedEntity.current)
+	const isHovered = useTrait(() => displayEntity, traits.Hovered)
+</script>
+
+{#if isHovered}
+	<HoveredEntity />
+
+	{#each linkedEntities.current as entity (entity)}
+		<LinkedHoveredEntity linkedEntity={entity} />
+	{/each}
+{/if}
