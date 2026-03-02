ad3142bd8a5420c80c93f8430d4cfa3f31d41530
Micheal Parks <micheal.parks@outlook.com>
2026-03-02 17:07:48 -0500

    export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export { default as MotionTools } from './components/App.svelte'
 
 // Plugins
 export { default as LassoTool } from './components/Lasso/Tool.svelte'
+export { default as PCD } from './components/PCD.svelte'
