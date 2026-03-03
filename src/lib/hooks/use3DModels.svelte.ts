5e861bc915f037c2be9b6784c5f1b510902d4f63
Micheal Parks <micheal.parks@outlook.com>
2026-03-03 16:55:17 -0500

    tweak

--- src/lib/hooks/use3DModels.svelte.ts
+++ src/lib/hooks/use3DModels.svelte.ts
@@ -60,8 +60,9 @@ export const provide3DModels = (partID: () => string) => {
 							if (isInstanceOf(object, 'Mesh')) {
 								const { material } = object
 
+								console.log(material)
 								if (isInstanceOf(material, 'MeshStandardMaterial')) {
-									material.roughness = 0.1
+									material.roughness = 0.3
 									material.metalness = 0.1
 								}
 							}
