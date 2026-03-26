59bb7f06f63cfc41e1a7d807843b04ee26373354
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2026-03-26 17:01:52 -0400

    Enable log filtering by level (#507)
    * upgrade svelte
    
    * add log levels
    
    * revert
    
    * Create cuddly-hounds-sit.md

--- src/lib/components/overlay/Logs.svelte
+++ src/lib/components/overlay/Logs.svelte
@@ -10,6 +10,12 @@
 	const logs = useLogs()
 
 	const isOpen = new PersistedState('logs-is-open', false)
+
+	let levels = new PersistedState('logs-selected-levels', {
+		info: true,
+		warn: true,
+		error: true,
+	})
 </script>
 
 <Portal id="dashboard">
@@ -44,34 +50,95 @@
 	title="Logs"
 	bind:isOpen={isOpen.current}
 	defaultSize={{ width: 240, height: 315 }}
+	resizable
 >
-	<div class="flex h-70 flex-col gap-2 overflow-auto p-3 text-xs">
-		{#each logs.current as log (log.uuid)}
-			<div>
-				<div class="flex flex-wrap items-center gap-1.5">
-					<div
-						class={[
-							'h-2 w-2 rounded-full',
-							{
-								'bg-danger-dark': log.level === 'error',
-								'bg-amber-300': log.level === 'warn',
-								'bg-blue-400': log.level === 'info',
-							},
-						]}
-					></div>
-					<div class="text-subtle-2">{log.timestamp}</div>
-				</div>
-				<div>
-					{#if log.count > 1}
-						<span class="mr-1 rounded bg-green-700 px-1 py-0.5 text-xs text-white">
-							{log.count}
-						</span>
-					{/if}
-					{log.message}
-				</div>
-			</div>
-		{:else}
-			No logs
-		{/each}
+	<div class="flex h-full flex-col">
+		<div class="flex gap-1 px-3 py-2">
+			<button
+				type="button"
+				class={[
+					'chip border px-2',
+					{
+						'border-danger-dark bg-danger-dark text-white hover:border-red-700 hover:bg-red-700':
+							levels.current.error,
+						'bg-light hover:bg-ghost-light hover:border-light border-light text-subtle-1':
+							!levels.current.error,
+					},
+				]}
+				onclick={() => {
+					levels.current.error = !levels.current.error
+				}}
+			>
+				error
+			</button>
+
+			<button
+				type="button"
+				class={[
+					'chip border',
+					{
+						'border-amber-400 bg-amber-400 text-white hover:border-amber-500 hover:bg-amber-500':
+							levels.current.warn,
+						'bg-light hover:bg-ghost-light hover:border-light border-light text-subtle-1':
+							!levels.current.warn,
+					},
+				]}
+				onclick={() => {
+					levels.current.warn = !levels.current.warn
+				}}
+			>
+				warn
+			</button>
+
+			<button
+				type="button"
+				class={[
+					'chip border',
+					{
+						'border-blue-400 bg-blue-400 text-white hover:border-blue-500 hover:bg-blue-500':
+							levels.current.info,
+						'bg-light hover:bg-ghost-light hover:border-light border-light text-subtle-1':
+							!levels.current.info,
+					},
+				]}
+				onclick={() => {
+					levels.current.info = !levels.current.info
+				}}
+			>
+				info
+			</button>
+		</div>
+
+		<div class="flex flex-col gap-2 overflow-auto px-3 pb-3 text-xs">
+			{#each logs.current as log (log.uuid)}
+				{#if levels.current[log.level]}
+					<div>
+						<div class="flex flex-wrap items-center gap-1.5">
+							<div
+								class={[
+									'h-2 w-2 rounded-full',
+									{
+										'bg-danger-dark': log.level === 'error',
+										'bg-amber-300': log.level === 'warn',
+										'bg-blue-400': log.level === 'info',
+									},
+								]}
+							></div>
+							<div class="text-subtle-2">{log.timestamp}</div>
+						</div>
+						<div>
+							{#if log.count > 1}
+								<span class="mr-1 rounded bg-green-700 px-1 py-0.5 text-xs text-white">
+									{log.count}
+								</span>
+							{/if}
+							{log.message}
+						</div>
+					</div>
+				{/if}
+			{:else}
+				No logs
+			{/each}
+		</div>
 	</div>
 </FloatingPanel>
