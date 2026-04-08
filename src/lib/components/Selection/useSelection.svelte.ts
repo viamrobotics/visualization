2d88c0688cbe0a150fbf8d18abb4f45793f7f5cf
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-04-08 09:12:05 -0400

    lint

--- src/lib/components/Selection/useSelection.svelte.ts
+++ src/lib/components/Selection/useSelection.svelte.ts
@@ -11,33 +11,33 @@ const key = Symbol('selection-context')
 
 interface SelectionContext {
 	current: QueryResult<[Trait<() => boolean>]>
-    selectionCallbacks: ((entity: Entity) => void)[]
-    clearSelections: () => void
-    registerSelectionCallback: (callback: (entity: Entity) => void) => void
+	selectionCallbacks: ((entity: Entity) => void)[]
+	clearSelections: () => void
+	registerSelectionCallback: (callback: (entity: Entity) => void) => void
 }
 
 export const provideSelection = () => {
-    const callbacks = $state.raw<((entity: Entity) => void)[]>([])
-    const world = useWorld()
+	const callbacks = $state.raw<((entity: Entity) => void)[]>([])
+	const world = useWorld()
 
-    const ctx = setContext<SelectionContext>(key, {
-        get current() {
-            return world.query(selectionTraits.SelectionEnclosedPoints)
-        },
-        get selectionCallbacks() {
-            return callbacks
-        },
-        clearSelections() {
-            for (const entity of world.query(selectionTraits.SelectionEnclosedPoints)) {
-                if (world.has(entity)) {
-                    entity.destroy()
-                }
-            }
-        },
-        registerSelectionCallback(callback) {
-            callbacks.push(callback)
-        },
-    })
+	const ctx = setContext<SelectionContext>(key, {
+		get current() {
+			return world.query(selectionTraits.SelectionEnclosedPoints)
+		},
+		get selectionCallbacks() {
+			return callbacks
+		},
+		clearSelections() {
+			for (const entity of world.query(selectionTraits.SelectionEnclosedPoints)) {
+				if (world.has(entity)) {
+					entity.destroy()
+				}
+			}
+		},
+		registerSelectionCallback(callback) {
+			callbacks.push(callback)
+		},
+	})
 	return ctx
 }
 
