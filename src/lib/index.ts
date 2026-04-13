f695e8bdf26fe6c9c3a67098539f5f28bb1cd88e
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-04-13 12:06:44 -0400

    ts check

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,7 +8,7 @@ export { default as PCD } from './components/PCD.svelte'
 export * as relations from './ecs/relations'
 export * as traits from './ecs/traits'
 export * as selectionTraits from './components/Selection/traits'
-export { useSelectionPlugin as useSelection } from './components/Selection/useSelectionPlugin.svelte.ts'
+export { useSelectionPlugin as useSelection } from './components/Selection/useSelectionPlugin.svelte'
 export { default as FloatingPanel } from './components/overlay/FloatingPanel.svelte'
 
 export { provideWorld, useWorld } from './ecs/useWorld'
